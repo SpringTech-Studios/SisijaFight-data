c6c0f91d6d46c05f0938293458868553b0de37d8
Lucas G <lucas.gola@icloud.com>
2024-02-08 20:45:20 +0100

    Updated Executable, added Whispercpp-model, and added french and Italiano translation

--- application/windows/cmake_install.cmake
+++ application/windows/cmake_install.cmake
@@ -41,6 +41,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("C:/Users/hanna/Desktop/Sisija/SisijaFight-data/application/windows/lib/whispercpp/cmake_install.cmake")
   include("C:/Users/hanna/Desktop/Sisija/SisijaFight-data/application/windows/lib/portaudio/cmake_install.cmake")
+  include("C:/Users/hanna/Desktop/Sisija/SisijaFight-data/application/windows/lib/curl/cmake_install.cmake")
 
 endif()
 
